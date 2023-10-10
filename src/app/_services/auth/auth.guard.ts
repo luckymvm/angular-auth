d650058a1f6819bca8afcf35e306caa3fca07261
luckymvm <canman4956@gmail.com>
2023-10-10 17:16:40 +0500

    Implemented canMatch instead of canActivate

--- src/app/_services/auth/auth.guard.ts
+++ src/app/_services/auth/auth.guard.ts
@@ -1,8 +1,21 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, CanMatchFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {AuthService} from "./auth.service";
+import {toObservable} from "@angular/core/rxjs-interop";
+import {filter, map} from "rxjs";
 
-export const authGuard: CanActivateFn = (route, state) => {
-  const auth = inject(AuthService);
-  return auth.status() === 'authenticated';
+export const authGuard = (type: 'protected' | 'unprotected'): CanMatchFn => {
+    return () => {
+        const auth = inject(AuthService);
+        const router = inject(Router);
+
+        return toObservable(auth.isAuthenticated).pipe(
+            filter(() => !auth.isAuthenticating()),
+            map((isAuthenticated) => {
+                if ((type === 'unprotected' && !isAuthenticated) || (type === 'protected' && isAuthenticated))
+                    return true;
+                return router.parseUrl('/signin');
+            })
+        );
+    }
 };
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {SigninComponent} from "./signin/signin.component";
 import {RegisterComponent} from "./register/register.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {authGuard} from "./_services/auth/auth.guard";
 
 const routes: Routes = [
-  {path: 'signin', component: SigninComponent},
-  {path: 'signup', component:  RegisterComponent},
-  {path: 'dashboard', component:  DashboardComponent},
+  {path: 'dashboard', component:  DashboardComponent, canMatch: [authGuard('protected')]},
+  {path: 'signin', component: SigninComponent, canMatch: [authGuard('unprotected')]},
+  {path: 'signup', component:  RegisterComponent, canMatch: [authGuard('unprotected')]},
 ];
 
 @NgModule({

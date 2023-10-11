4096a8cb3f4c82f233bb22b8e880c14cd5825ef2
luckymvm <canman4956@gmail.com>
2023-10-11 16:11:49 +0500

    Redirect when visiting a non-existing route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {SigninComponent} from "./signin/signin.component";
 import {RegisterComponent} from "./register/register.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {authGuard} from "./_services/auth/auth.guard";
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
   {path: 'dashboard', component:  DashboardComponent, canMatch: [authGuard('protected')]},
   {path: 'signin', component: SigninComponent, canMatch: [authGuard('unprotected')]},
   {path: 'signup', component:  RegisterComponent, canMatch: [authGuard('unprotected')]},
+  {path: '**', component:  AppComponent},
 ];
 
 @NgModule({

9cd48a699b43eb7959b7fe0bbc3d7e8eba356a0e
luckymvm <canman4956@gmail.com>
2023-10-16 13:41:48 +0500

    Added page titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,9 @@ import {authGuard} from "./_services/auth/auth.guard";
 import {AppComponent} from "./app.component";
 
 const routes: Routes = [
-  {path: 'dashboard', component:  DashboardComponent, canMatch: [authGuard('protected')]},
-  {path: 'signin', component: SigninComponent, canMatch: [authGuard('unprotected')]},
-  {path: 'signup', component:  RegisterComponent, canMatch: [authGuard('unprotected')]},
+  {path: 'dashboard', component:  DashboardComponent, canMatch: [authGuard('protected')], title: 'Dashboard'},
+  {path: 'signin', component: SigninComponent, canMatch: [authGuard('unprotected')], title: 'Log in'},
+  {path: 'signup', component:  RegisterComponent, canMatch: [authGuard('unprotected')], title: 'Sign Up'},
   {path: '**', redirectTo: 'dashboard'},
 ];
 
